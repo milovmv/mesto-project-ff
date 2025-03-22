f6d7b57e1249cb6a3265b74337fab5fad09a6d95
milovmv <milov@phystech.edu>
2025-03-22 14:48:32 +0300

    first  try

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+function addCard(cardElement) {
+    for (let i = 0; i<initialCards.length; i++) {
+        const cardTemplate = document.querySelector('#card-template').content;
+        cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+        
+        const cardsOnPage = document.querySelector('.places__list');
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+        console.log(deleteButton);
+    
+        cardElement.querySelector('.card__image').src = initialCards[i].link;
+        cardElement.querySelector('.card__image').alt = initialCards[i].name;
+        cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+        console.log(cardElement);
+    
+        cardsOnPage.append(cardElement);
 
-// @todo: DOM узлы
+        deleteButton.addEventListener('click', function () {
+            const listItem = deleteButton.closest('.card');
+            listItem.remove();
+        });
+    }
+}
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+addCard()
