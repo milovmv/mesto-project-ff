834408b7545a5d70c6bb0bdebb5ab205278f0f80
milovmv <milov@phystech.edu>
2025-03-23 16:25:23 +0300

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,34 @@
-function addCard(cardElement) {
+
+function deleteItem() {
+    const listItem = document.querySelector('.card');
+    listItem.remove();
+};
+
+function createCard(deletefunc) {
+    const cardMassive = [];
     for (let i = 0; i<initialCards.length; i++) {
         const cardTemplate = document.querySelector('#card-template').content;
-        cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
         
-        const cardsOnPage = document.querySelector('.places__list');
         const deleteButton = cardElement.querySelector('.card__delete-button');
-        console.log(deleteButton);
-    
-        cardElement.querySelector('.card__image').src = initialCards[i].link;
-        cardElement.querySelector('.card__image').alt = initialCards[i].name;
-        cardElement.querySelector('.card__title').textContent = initialCards[i].name;
-        console.log(cardElement);
-    
-        cardsOnPage.append(cardElement);
+        const cardImage = cardElement.querySelector('.card__image');
+        const cardTitle = cardElement.querySelector('.card__title');
+        
+        cardImage.src = initialCards[i].link;
+        cardImage.alt = initialCards[i].name;
+        cardTitle.textContent = initialCards[i].name;
+        
 
-        deleteButton.addEventListener('click', function () {
-            const listItem = deleteButton.closest('.card');
-            listItem.remove();
-        });
+        deleteButton.addEventListener('click', deletefunc);
+        cardMassive[i] = cardElement;
     }
+
+    return cardMassive;
 }
 
+createCard(deleteItem).forEach(function(item) {
+    const cardsOnPage = document.querySelector('.places__list');
+    cardsOnPage.append(item);
+});
 
-addCard()
+
